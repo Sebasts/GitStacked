5345aefdfe72a200d773507f96288ef076c4f80e
Paul Pistorius <jpistoriusp@gmail.com>
2017-06-03 14:21:36 -0600

    user entity

--- JPAGitStacked/src/entities/User.java
+++ JPAGitStacked/src/entities/User.java
@@ -12,11 +12,26 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	private String fname;
+	private String lname;
 	private int weight;
 	private int heightFeet;
 	private int heightInch;
 	private String username;
 	private String password;
+	private UserType usertype;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
 	public int getWeight() {
 		return weight;
 	}
@@ -58,8 +73,9 @@ public class User {
 	}
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", weight=" + weight + ", heightFeet=" + heightFeet + ", heightInch=" + heightInch
-				+ ", username=" + username + ", password=" + password + ", usertype=" + usertype + "]";
+		return "User [id=" + id + ", fname=" + fname + ", lname=" + lname + ", weight=" + weight + ", heightFeet="
+				+ heightFeet + ", heightInch=" + heightInch + ", username=" + username + ", password=" + password
+				+ ", usertype=" + usertype + "]";
 	}
-	private UserType usertype;
+	
 }
