2cbdad5c7c6ecff7dfe041a7c35eaac3d3f6e1e8
Nancy Tran <nancytran@SDMacBookPro3-2.local>
2017-06-03 15:03:41 -0600

    deleted my hard work

--- GitStacked/src/data/PersistenceDAOImpl.java
+++ GitStacked/src/data/PersistenceDAOImpl.java
@@ -6,7 +6,7 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import Entities.User;
+import entities.User;
 
 
 @Transactional
