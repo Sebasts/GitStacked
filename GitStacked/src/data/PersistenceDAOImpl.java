ff5be6318030587aa79453d3cf262b3c768c1cfa
Sebastian Hellsing <sebastsing@gmail.com>
2017-06-05 12:35:37 -0600

    a

--- GitStacked/src/data/PersistenceDAOImpl.java
+++ GitStacked/src/data/PersistenceDAOImpl.java
@@ -6,9 +6,9 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import entities.Exercise;
-import entities.LoginUserType;
-import entities.User;
+
+
+
 
 
 @Transactional
